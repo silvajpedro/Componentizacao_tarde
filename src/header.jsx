af94649f878b4f409684c9120cd7f77e7d457608
silvajpedro <silvajpro.31@gmail.com>
2023-09-15 16:26:34 -0300

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,19 @@
+
+
+function Header(){
+    return(
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Sobre</li>
+                    <li>Contatos</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Header
